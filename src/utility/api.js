5dcd441a083e2f680d6d664f665a8e160a2cc82b
jahongir7 <jahongir95119@gmail.com>
2022-06-26 12:59:45 +0500

    api changed

--- src/utility/api.js
+++ src/utility/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://onkolagiya.herokuapp.com/',
+  baseURL: 'https://bilim-coin.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
   }
